35b2ace82389a98ba0cd15d045d75a1a9c156c5c
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-02-13 10:34:26 +0600

    commit

--- database/settings.py
+++ database/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v^3fa#@!xl54ayfauhzljnyj@@p*co&i+b9umtpjf2xdi4$*1m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app"]
 
